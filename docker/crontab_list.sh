84549bb85b868c27c8e4cab0410d2e8025041f9f
zsw503 <74197759+zsw503@users.noreply.github.com>
2021-08-01 13:54:31 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -5,8 +5,8 @@
 
 ##############活动##############
 #财富岛
-0 20 * * * node /scripts/jd_cfd.js >> /scripts/logs/jd_cfd.log 2>&1
+0 10,20 * * * node /scripts/jd_cfd.js >> /scripts/logs/jd_cfd.log 2>&1
 #财富岛提现
 30 59 11,23 * * * node /scripts/jd_cfd_withdraw.js >> /scripts/logs/jd_cfd_withdraw.log 2>&1
 #极速版任务
-59 11,23 * * * node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1
+10 2,8,16 * * * node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1
