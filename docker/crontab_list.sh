fb48685f9b94f4d1e4313ed0c0a67477f981c03a
zsw503 <74197759+zsw503@users.noreply.github.com>
2021-08-22 11:02:26 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -6,3 +6,5 @@
 ##############活动##############
 #极速版任务
 10 2,8,16 * * * node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1
+#资产综合推送
+0 12 * * * node /scripts/jd_bean_change.js >> /scripts/logs/jd_bean_change.log 2>&1
