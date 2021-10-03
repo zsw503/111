408fe3dba0506487e82f7532189dfd2a29bf6f7e
zsw503 <74197759+zsw503@users.noreply.github.com>
2021-10-03 17:43:55 +0800

    Add files via upload

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,7 +1,7 @@
-# 每天的23:00分清理一次日志(互助码不清理，proc_file.sh对该文件进行了去重)
-0 23 */1 * * find /scripts/logs -name '*.log' | grep -v 'sharecodeCollection' | xargs rm -rf
-# 收集助力码
-30 2 * * * sh /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
+# 每3天的23:50分清理一次日志(互助码不清理，proc_file.sh对该文件进行了去重)
+50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecodeCollection' | xargs rm -rf
+#收集助力码
+#30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
 
 ##############一对一推送活动##############
 # 京东资产变动通知
